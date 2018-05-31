194097ba411cdf99620fb98773c1fdc02ecc5e0c
robertolago <robertolagob@gmail.com>
2018-05-31 15:39:45 -0500

    Chore:Create a page that displays "Hello World!" when a user visits /hello (using servlets), if the user pass a name show Hello + name

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -9,8 +9,18 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        String name=request.getParameter("name");
+
+        if (name==null){
+            name="World";
+        }
+
+        response.getWriter().println("<H1>Hello"+name+"</H1>");
+        response.getWriter().println("<p>Enter your name:</p>");
+        response.getWriter().println("<form action=\"/hello\">");
+        response.getWriter().println("<input type=\"text\" name=\"name\">");
+        response.getWriter().println("</form>");
+
     }
 }
 
