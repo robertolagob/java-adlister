e653f9d299929462365bc0117b3eaa001c8a1de3
robertolago <robertolagob@gmail.com>
2018-06-04 22:49:49 -0500

    Chore: controlling a session using a HttpSession variable: checking on doGet and doPos method if the session was previously opened and keeping it open.

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,22 +3,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession user=request.getSession();
+        if ((user.getAttribute("loged")==null)){
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
+        else {
+            response.sendRedirect("/profile");
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+//
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession user=request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
-
-        if (validAttempt) {
-            response.sendRedirect("/profile");
-        } else {
+        if ((validAttempt)&&(user.getAttribute("loged")==null)){
+                user.setAttribute("loged","yes");
+                request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request,response);
+            }
+         else  {
             response.sendRedirect("/login");
         }
     }
