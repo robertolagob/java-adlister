b78406cb4aad2c9b0182eb71d0e4239bebd00b56
robertolago <robertolagob@gmail.com>
2018-06-07 11:59:37 -0500

    fixed files configuration

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,8 +1,8 @@
 public class Config {
 
 
-        private String user="root";
-        private String password="codeup";
+        private String user="";
+        private String password="";
         private String url="jdbc:mysql://localhost:3306/adlister_db?serverTimezone=UTC";
 
         public String getUser() {
