59cc0d9c0098b4414817380b9f9e8e1354d73e06
robertolago <robertolagob@gmail.com>
2018-06-07 10:33:40 -0500

    Feature: correcting files configuration

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -9,5 +9,6 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println("<h1>Hello, World!</h1>");
 
+
     }
 }
